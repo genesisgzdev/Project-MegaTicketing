83f68c4b10a4ec9428806dac0ec5435e1c92cec4
Genesis <genzt.dev@pm.me>
2026-08-20 11:54:41 -0300

    fix: declare Cloudflare provider in infrastructure root

--- infra/versions.tf
+++ infra/versions.tf
@@ -10,7 +10,12 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "kubernetes" {}
+provider "cloudflare" {}
